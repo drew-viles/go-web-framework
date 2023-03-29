e3766cfb8245e95afd5cb731b2e42985ba09c311
Drew Viles <vilesy@gmail.com>
2023-03-29 21:48:10 +0100

    patch: license updates

--- validation/validator.go
+++ validation/validator.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2023 Drew Viles.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package validation
 
 import (
@@ -31,7 +47,7 @@ type Validator struct {
 }
 
 // New creates and returns a new validator
-//TODO: needs to be more dynamic as the hardcoded validations at the bottom are not ideal.
+// TODO: needs to be more dynamic as the hardcoded validations at the bottom are not ideal.
 func New() *Validator {
 	log.Println("Setting up translation and validation")
 	v := &Validator{}
